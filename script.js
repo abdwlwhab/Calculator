9d50c71ff508822dedb2683b5b1d0b81a03b9539
Abdelwhabali <155674851+abdwlwhab@users.noreply.github.com>
2025-11-02 19:57:38 +0200

    added basic functions

---
+++ script.js
@@ -0,0 +1,10 @@
+const add = function (num1,num2)
+{
+    return num1+num2;
+}
+// =========================================
+const subtract = function (num1,num2)
+{
+    return num1-num2;
+}
+console.log(subtract(4,2))
