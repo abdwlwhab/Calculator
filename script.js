51a6796db22c3765292f9b897de4b6660acf9866
Abdelwhabali <155674851+abdwlwhab@users.noreply.github.com>
2025-11-02 20:26:21 +0200

    made a prototype calculator

--- script.js
+++ script.js
@@ -7,4 +7,31 @@ const subtract = function (num1,num2)
 {
     return num1-num2;
 }
-console.log(subtract(4,2))
+// ==========================================
+const multiply = function (num1,num2)
+{
+    return num1*num2;
+}
+// ==========================================
+const divide = function (num1,num2)
+{
+    return num1/num2;
+}
+// ================================================================================
+let operator; 
+let num1 ;
+let num2 ;
+// ===================================================================================
+num1=Number(prompt('enter a umber',0));
+operator= prompt('enter an operator ','+');
+num2 =Number(prompt('enter a umber',0));
+
+const calc =  function (n1,oper,n2)
+{
+  if (oper === "+") return add(n1, n2);
+  if (oper === "-") return subtract(n1, n2);
+  if (oper === "*") return multiply(n1, n2);
+  if (oper === "/") return divide(n1, n2);
+  return "Invalid operator";
+}
+console.log(calc(num1, operator, num2))
